a3ae80152fe49f88cc6a46bfd220ebebd2ee6b75
mattrichmo <matt.richmond3@gmail.com>
2023-08-28 20:34:13 -0700

    edited footer grid

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,81 +6,82 @@ export function SiteFooter() {
   return (
     <footer className="border-t bg-background">
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
-        <div className=" flex-col space-y-8 sm:flex-row sm:items-start sm:justify-between sm:space-y-0">
-          <div className=" items-center space-x-2 sm:space-x-4">
-            <Icons.logo className="h-8 w-8" />
-            <span className="inline-block text-lg font-bold">{siteConfig.name}</span>
-          
-          <p className="mt-4 max-w-xs text-sm sm:text-base">
-            &quot;Learn The Rules Like a Pro, So You Can Break Them Like an Artist.&quot; <br /> - Pablo Picasso or maybe Buddha
-          </p>
-          </div>
-        </div>
-        <div className="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-3">
-          <div>
-            <p className="font-medium">Projects</p>
-            <ul className="mt-6 space-y-4 text-sm">
-              <li>
-                <Link href="/projects" className=" transition hover:opacity-75">
-                  What I&apos;m Working On Now
-                </Link>
-              </li>
-              <li>
-                <Link href="/projects/code" className=" transition hover:opacity-75">
-                  Code 
-                </Link>
-              </li>
-              <li>
-                <Link href="/projects/design" className=" transition hover:opacity-75">
-                  Design
-                </Link>
-              </li>
-              <li>
-                <Link href="/projects/creative" className=" transition hover:opacity-75">
-                  Creative
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p className="font-medium">Me</p>
-            <ul className="mt-6 space-y-4 text-sm">
-              <li>
-                <Link href="/about" className=" transition hover:opacity-75">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link href="/contact" className=" transition hover:opacity-75">
-                  Contact
-                </Link>
-              </li>
-              <li>
-                <Link href="/about/film-cv" className=" transition hover:opacity-75">
-                  Film CV
-                </Link>
-              </li>
-              <li>
-                <Link href="/link-list" className=" transition hover:opacity-75">
-                  Link List                                    
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p className="font-medium">Photography</p>
-            <ul className="mt-6 space-y-4 text-sm">
-              <li>
-                <Link href="/photo/selected-galleries" className=" transition hover:opacity-75">
-                  Favourite Photo Galleries             
-                </Link>
-              </li>
-              <li>
-                <Link href="/photo/external-galleries" className=" transition hover:opacity-75">
-                  External Galleries              
-                </Link>
-              </li>
-            </ul>
+        <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between sm:space-y-0">
+<div className="grid flex-col items-center sm:flex-row sm:items-start sm:justify-between sm:space-x-4 sm:space-y-4">
+  <div className="flex items-center space-x-2">
+    <Icons.logo className="h-8 w-8" />
+    <span className="inline-block text-lg font-bold">{siteConfig.name}</span>
+  </div>
+  <p className="mt-4  max-w-xs text-left text-sm sm:text-left sm:text-base">
+    &quot;Learn The Rules Like a Pro, So You Can Break Them Like an Artist.&quot; <br /> - Pablo Picasso or maybe Buddha
+  </p>
+</div>
+          <div className="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-3">
+            <div>
+              <p className="font-medium">Projects</p>
+              <ul className="mt-6 space-y-4 text-sm">
+                <li>
+                  <Link href="/projects" className=" transition hover:opacity-75">
+                    What I&apos;m Working On Now
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/projects/code" className=" transition hover:opacity-75">
+                    Code 
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/projects/design" className=" transition hover:opacity-75">
+                    Design
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/projects/creative" className=" transition hover:opacity-75">
+                    Creative
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-medium">Me</p>
+              <ul className="mt-6 space-y-4 text-sm">
+                <li>
+                  <Link href="/about" className=" transition hover:opacity-75">
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/contact" className=" transition hover:opacity-75">
+                    Contact
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/about/film-cv" className=" transition hover:opacity-75">
+                    Film CV
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/link-list" className=" transition hover:opacity-75">
+                    Link List                                    
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-medium">Photography</p>
+              <ul className="mt-6 space-y-4 text-sm">
+                <li>
+                  <Link href="/photo/selected-galleries" className=" transition hover:opacity-75">
+                    Favourite Photo Galleries             
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/photo/external-galleries" className=" transition hover:opacity-75">
+                    External Galleries              
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <p className="mt-8 text-xs text-gray-500">
