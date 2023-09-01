aabfde53f472a2d1ac475ae2474e064510f8738b
mattrichmo <matt.richmond3@gmail.com>
2023-09-01 13:45:33 -0700

    trying another by using getstatciprops

--- app/api/folder-images/route.ts
+++ app/api/folder-images/route.ts
@@ -1,48 +1,48 @@
-import { NextRequest, NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
 import sizeOf from 'image-size';
-import { string } from 'zod';
 
 const getImageFiles = (imgFolderPath: string) => {
-  const publicFolderPath = path.join(process.cwd(), './public/');
-  const imgFolder = path.join(publicFolderPath + imgFolderPath);
+  const publicFolderPath = path.join(process.cwd(), 'public');
+  const imgFolder = path.join(publicFolderPath, imgFolderPath);
   const files = fs.readdirSync(imgFolder);
-  
+
   console.log('Step 1: Reading files from folder...');
-  
+
   return files.map((file) => {
-    const filePath = imgFolderPath + file;
+    const filePath = path.join(imgFolder, file);
     const alt = path.basename(file, path.extname(file));
-    const dimensions = sizeOf(path.join(imgFolder, file));
-    
+    const dimensions = sizeOf(filePath);
+
     console.log('Step 2: Processing file:', file);
-    
+
     return {
-      src: filePath, // Use the constructed file path
+      src: `/${imgFolderPath}/${file}`, // Use the constructed file path
       alt,
       dimensions,
     };
   });
 };
 
-export async function GET(req: NextRequest) {
+export async function getStaticProps() {
   try {
-    const { method } = req;
-    const imgFolderPath = req.nextUrl.searchParams.get('imgfolderpath');
-    
-    if (method === 'GET' && imgFolderPath) {
-      console.log('Step 3: Handling GET request...');
-      const images = getImageFiles(imgFolderPath);
-      console.log('Step 4: Sending JSON response...');
-      return NextResponse.json(images);
-    } else {
-      console.log('Step 3: Method Not Allowed');
-      return NextResponse.json({ error: 'Method Not Allowed' });
-    }
+    console.log('Step 3: Handling GET request...');
+    const imgFolderPath = 'your_image_folder_path';
+    const images = getImageFiles(imgFolderPath);
+
+    console.log('Step 4: Sending JSON response...');
+    return {
+      props: {
+        images,
+      },
+    };
   } catch (error) {
     console.error('Error fetching images:', error);
     console.log('Step 3: An error occurred while fetching images.');
-    return NextResponse.json({ error: 'An error occurred while fetching images.' });
+    return {
+      props: {
+        error: 'An error occurred while fetching images.',
+      },
+    };
   }
 }
