a799c2c2d3d81bb7672787ccd32b8d5e0fa6bb30
mattrichmo <matt.richmond3@gmail.com>
2023-09-01 10:01:44 -0700

    removed unused code

--- components/gallery/getImgDimensions.tsx
+++ components/gallery/getImgDimensions.tsx
@@ -1,10 +1,10 @@
-export function getImageDimensions(filePath) {
-    const image = new Image();
+export function getImageDimensions(filePath: string) {
+/*     const image = new Image();
     image.src = filePath;
   
     return {
       width: image.width,
       height: image.height,
-    };
+    }; */
   }
   
