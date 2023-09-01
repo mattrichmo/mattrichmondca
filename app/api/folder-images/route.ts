008c38b6bac6b433ed308c472c90ff42b821142e
mattrichmo <matt.richmond3@gmail.com>
2023-09-01 12:42:14 -0700

    added error handling because i cant figure out whyt he eff it works in dev but not prod

--- app/api/folder-images/route.ts
+++ app/api/folder-images/route.ts
@@ -1,21 +1,22 @@
-"use server"
 import { NextRequest, NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
 import sizeOf from 'image-size';
 import { string } from 'zod';
 
-
-
 const getImageFiles = (imgFolderPath: string) => {
-  const publicFolderPath = path.join(process.cwd(), './public');
+  const publicFolderPath = path.join(process.cwd(), 'public');
   const imgFolder = path.join(publicFolderPath, imgFolderPath);
   const files = fs.readdirSync(imgFolder);
+  
+  console.log('Step 1: Reading files from folder...');
   
   return files.map((file) => {
     const filePath = imgFolderPath + file;
     const alt = path.basename(file, path.extname(file));
     const dimensions = sizeOf(path.join(imgFolder, file));
+    
+    console.log('Step 2: Processing file:', file);
     
     return {
       src: filePath, // Use the constructed file path
@@ -31,13 +32,17 @@ export async function GET(req: NextRequest) {
     const imgFolderPath = req.nextUrl.searchParams.get('imgfolderpath');
     
     if (method === 'GET' && imgFolderPath) {
+      console.log('Step 3: Handling GET request...');
       const images = getImageFiles(imgFolderPath);
+      console.log('Step 4: Sending JSON response...');
       return NextResponse.json(images);
     } else {
+      console.log('Step 3: Method Not Allowed');
       return NextResponse.json({ error: 'Method Not Allowed' });
     }
   } catch (error) {
     console.error('Error fetching images:', error);
+    console.log('Step 3: An error occurred while fetching images.');
     return NextResponse.json({ error: 'An error occurred while fetching images.' });
   }
 }
--- components/gallery/gallery1.tsx
+++ components/gallery/gallery1.tsx
@@ -7,12 +7,47 @@ import 'yet-another-react-lightbox/styles.css';
 interface ImageData {
   src: string;
   alt: string;
-  dimensions: { width: number; height: number };
+  dimensions: { width: number; height: number } | null; // Allow dimensions to be null
 }
 
 interface Gallery1Props {
   imgFolderPath: string;
 }
+
+class ErrorBoundary extends React.Component<{ children: React.ReactNode }, { hasError: boolean; error: Error | null }> {
+  state = {
+    hasError: false,
+    error: null,
+  };
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state to show the fallback UI.
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    // You can log the error to an error reporting service
+    console.error('Error caught in ErrorBoundary:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Use the 'unknown' type for error
+      const error = this.state.error as unknown;
+
+      // You can render any custom fallback UI here
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <p>{String(error)}</p> {/* Use String() to convert to string */}
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+
 
 export default function Gallery1({ imgFolderPath }: Gallery1Props) {
   const [index, setIndex] = React.useState(-1);
@@ -25,10 +60,10 @@ export default function Gallery1({ imgFolderPath }: Gallery1Props) {
         const data: ImageData[] = await response.json();
         console.log(data); // Add this line after fetching data
 
-
         setImages(data); // Update the state after fetching data
       } catch (error) {
         console.error('Error fetching images:', error);
+        throw error; // Rethrow the error to trigger the error boundary
       }
     }
 
@@ -37,30 +72,32 @@ export default function Gallery1({ imgFolderPath }: Gallery1Props) {
 
   // Now, render the component with the updated state
   return (
-    <div className="mx-auto mt-24 max-w-screen-xl px-8">
-      <PhotoAlbum
-        layout="rows"
-        photos={images.map((image) => ({
-          src: image.src,
-          alt: image.alt,
-          width: image.dimensions.width,
-          height: image.dimensions.height,
-        }))}
-        targetRowHeight={150}
-        onClick={({ index: current }) => setIndex(current)}
-      />
+    <ErrorBoundary>
+      <div className="mx-auto mt-24 max-w-screen-xl px-8">
+        <PhotoAlbum
+          layout="rows"
+          photos={images.map((image) => ({
+            src: image.src,
+            alt: image.alt,
+            width: image.dimensions?.width || 0, // Use optional chaining
+            height: image.dimensions?.height || 0, // Use optional chaining
+          }))}
+          targetRowHeight={150}
+          onClick={({ index: current }) => setIndex(current)}
+        />
 
-      <Lightbox
-        index={index}
-        slides={images.map((image) => ({
-          src: image.src,
-          alt: image.alt,
-          width: image.dimensions?.width || 0,
-          height: image.dimensions?.height || 0,
-        }))}
-        open={index >= 0}
-        close={() => setIndex(-1)}
-      />
-    </div>
+        <Lightbox
+          index={index}
+          slides={images.map((image) => ({
+            src: image.src,
+            alt: image.alt,
+            width: image.dimensions?.width || 0, // Use optional chaining
+            height: image.dimensions?.height || 0, // Use optional chaining
+          }))}
+          open={index >= 0}
+          close={() => setIndex(-1)}
+        />
+      </div>
+    </ErrorBoundary>
   );
 }
