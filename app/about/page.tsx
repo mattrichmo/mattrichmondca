675ca5f0e3b79528a862d511211fd216f83750ec
mattrichmo <matt.richmond3@gmail.com>
2023-11-20 11:02:47 -0800

    updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,35 +3,28 @@ import Link from "next/link";
 
 export default function AboutPage() {
   return (
-    <section className="mx-12 flex h-screen flex-col items-center gap-6 pb-4 pt-6 sm:ml-4 md:py-10">
-      <div className="xs:mt-6">
-        <div className="flex max-w-[980px] flex-col items-start gap-4">
-          <h1 className="ml-12 mt-8 text-left text-2xl leading-tight tracking-tighter md:text-3xl" style={{ lineHeight: "1.5", fontFamily: "ETBembo, Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif", fontWeight: "normal" }}>
-            Hi, I&apos;m Matt. 
+    <section className="mx-4 flex flex-col items-center gap-6 pb-8 pt-6 sm:mx-6 sm:ml-0 md:mx-12 md:py-10">
+      <div className="xs:mt-6 max-w-[980px]">
+        <div className="flex flex-col items-start gap-4">
+          <h1 className="ml-0 mt-8 text-left text-2xl leading-tight tracking-tighter md:text-3xl" style={{ lineHeight: "1.5", fontFamily: "ETBembo, Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif", fontWeight: "normal" }}>
+            Hi, I&apos;m Matt.
           </h1>
-          <h2 className="text-md ml-12 mt-2 leading-tight tracking-tighter md:text-2xl" style={{ lineHeight: "1.5", fontFamily: "ETBembo, Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif", fontWeight: "normal" }}>
+          <h2 className="text-md ml-0 mt-2 leading-tight tracking-tighter md:text-2xl" style={{ lineHeight: "1.5", fontFamily: "ETBembo, Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif", fontWeight: "normal" }}>
             I am a Grip in the film industry, and I love photography, design, and code.
           </h2>
-          <p className="ml-12 mt-6 max-w-[800px] text-lg" style={{ fontFamily: "ETBembo, Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif", fontWeight: "normal" }}>
+          <p className="ml-0 mt-6 max-w-[800px] text-lg" style={{ fontFamily: "ETBembo, Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif", fontWeight: "normal" }}>
             Throughout my journey, I&apos;ve truly embodied the roles of a &quot;Creator&quot; and &quot;Builder&quot;, traits that have defined my path. My recent experiences have been quite diverse; during the pandemic, I immersed myself in the Vancouver film industry as a Grip, contributing to the magic of visual storytelling behind the scenes. Before that, I found myself engaged in brand-building endeavors within the dynamic landscape of Aurora Cannabis. Reflecting on my journey, I ventured into entrepreneurship with my own company, a venture that might not have reached the heights I envisioned, but one that undoubtedly enriched me with invaluable lessons.
           </p>
-          <p className="ml-12 mt-6 max-w-[800px] text-lg" style={{ fontFamily: "ETBembo, Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif", fontWeight: "normal" }}>
+          <p className="ml-0 mt-6 max-w-[800px] text-lg" style={{ fontFamily: "ETBembo, Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif", fontWeight: "normal" }}>
             My earlier days were marked by a deep involvement in creating brands, an arena where my passion for shaping concepts into something substantial truly shone. Each step has added to my arsenal of skills—whether it was the art of crafting compelling narratives in the film industry, establishing resonant brand identities, or navigating the complexities of running a business.
           </p>
-          <h2 className="text-md ml-12 mt-6 leading-tight tracking-tighter md:text-2xl" style={{ lineHeight: "1.5", fontFamily: "ETBembo, Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif", fontWeight: "normal" }}>
+          <h2 className="text-md ml-0 mt-6 leading-tight tracking-tighter md:text-2xl" style={{ lineHeight: "1.5", fontFamily: "ETBembo, Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif", fontWeight: "normal" }}>
             Happiest When Creating With Like-minded People
           </h2>
-          <p className="ml-12 mt-6 max-w-[800px] text-lg" style={{ fontFamily: "ETBembo, Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif", fontWeight: "normal" }}>
+          <p className="ml-0 mt-6 max-w-[800px] text-lg" style={{ fontFamily: "ETBembo, Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif", fontWeight: "normal" }}>
             As I gaze ahead, my enthusiasm for both creating and building remains undiminished. I am driven to channel my creativity and drive into future projects that embody this innate passion. There&apos;s an undeniable thrill in taking an idea, nurturing it, and bringing it into existence. With my background as a testament to my dedication, I eagerly anticipate the opportunity to continue this journey of transforming imagination into reality, leaving a trail of meaningful accomplishments along the way.
           </p>
         </div>
-      </div>
-      <div className="max-w mx-12 mb-12 mt-auto flex justify-center">
-        <Link href="/projects">
-          <Button className="bg-black px-4 py-2 font-semibold text-white">
-            View My Portfolio
-          </Button>
-        </Link>
       </div>
     </section>
   );
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,3 @@
-import ContactForm from "@/components/forms/contact-form";
 import SocialLinks from "@/components/links/social-link-list";
 import Link from "next/link";
 import { Lora } from 'next/font/google';
@@ -11,22 +10,18 @@ const lora = Lora({
 
 export default function ContactPage() {
   return (
-    <div className={`flex flex-col items-center ${lora.variable}`}>
-      <div className={`ml-12 mt-10 w-full sm:ml-4 md:w-2/3`}>
-        <h1 className="font-lora text-4xl font-semibold">Hit Me Up. </h1>
-        <h2 className="font-lora mt-2 text-2xl font-bold md:mr-24">
-          I&apos;m only using this page to test Form Submission, so probably better to email me directly
-        </h2>
+    <div className={`flex flex-col items-center ${lora.variable} mx-4 md:mx-12`}>
+      <div className={`ml-2 mt-10 w-full sm:ml-4 md:ml-12 md:w-2/3`}>
+        <h1 className="font-lora text-4xl font-semibold">Hit Me Up.</h1>
+        <p> If you have ideas, then let&apos;s get creative and explore what we can create together.</p>
         <Link href="mailto:hello@mattrichmond.ca">
-          <p className="mb-2 mt-4 hover:underline"> hello@mattrichmond.ca</p>
+          <p className="mb-2 mt-4 cursor-pointer text-2xl hover:underline">hello@mattrichmond.ca</p>
         </Link>
       </div>
-      <div className="mt-10 flex w-full flex-col md:w-2/3 md:flex-row " >
-        <div className="ml-4 w-full pr-0 sm:ml-4 md:w-1/2 md:pr-4 ">
+      <div className="mt-10 flex w-full flex-col md:w-2/3 md:flex-row">
+        <div className="ml-2 w-full pr-0 md:ml-4 md:w-1/2 md:pr-4">
+          <p className="mb-2 mt-4 cursor-pointer text-2xl hover:underline">Social Links:</p>
           <SocialLinks />
-        </div>
-        <div className="mx-auto flex w-full items-center justify-center pl-0 md:w-1/2"> {/* Updated line */}
-          <ContactForm />
         </div>
       </div>
     </div>
